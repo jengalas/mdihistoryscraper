6dd2ca29ef2e5d072b134f0b8b20494081322acc
Jennifer Galas <jennifer.galas@scranton.edu>
2018-05-10 18:13:54 -0400

    First commit

---
+++ parsedata.js
@@ -0,0 +1,24 @@
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+var requestURL = 'mdihistory.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var superHeroes = request.response;
+    showHeroes(superHeroes);
+  }
+
+  function showHeroes(jsonObj) {
+    var heroes = jsonObj['members'];
+    var myList = document.createElement('ul');
+
+    for (var i = 0; i < heroes.length; i++) {
+      
+        var listItem = document.createElement('li');
+        listItem.textContent = heroes[1].title;
+        myList.appendChild(listItem);
+      }
+  }
+  
